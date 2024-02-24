c48d4aff6c4b95bab1267b469a91140ef46cd3e0
RickyPWebDev <100093181+RickyPWebDev@users.noreply.github.com>
2024-02-24 11:36:32 +0000

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+
+// Sure there is a better way to do this but it allows us to dynamically show the years for both copy-write texts 
+
+document.getElementById('year').innerHTML = new Date().getFullYear();
+document.getElementById('year2').innerHTML = new Date().getFullYear();
+
+
