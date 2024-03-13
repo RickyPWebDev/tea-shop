1185ea54910896b5462aa550cacd61158aa39046
RickyPWebDev <100093181+RickyPWebDev@users.noreply.github.com>
2024-03-13 09:24:57 +0000

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -2,6 +2,6 @@
 // Sure there is a better way to do this but it allows us to dynamically show the years for both copy-write texts 
 
 document.getElementById('year').innerHTML = new Date().getFullYear();
-document.getElementById('year2').innerHTML = new Date().getFullYear();
+
 
 
